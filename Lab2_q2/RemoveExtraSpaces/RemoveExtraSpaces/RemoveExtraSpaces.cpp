3a7ab00ed873f982c8ac30fbe1a0f03e28dcf613
PLaGInc <vladimir.alt@inbox.ru>
2015-03-26 21:27:51 +0400

    Прошлый коммит

--- Lab2_q2/RemoveExtraSpaces/RemoveExtraSpaces/RemoveExtraSpaces.cpp
+++ Lab2_q2/RemoveExtraSpaces/RemoveExtraSpaces/RemoveExtraSpaces.cpp
@@ -4,8 +4,29 @@
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+string RemoveSpaces(string &str)
 {
+	for (int i = 0; i < str.length(); i++)
+	{
+	 if (str[0] == ' ' || (str[i] == ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')))
+		{
+			str.erase(i, 1);
+			i--;
+		}
+	}
+	cout << str << "There is no spaces" << endl;
+	return str;
+}
+
+int main(int argc, char* argv[])
+{
+	string str;
+	if (argc != 2) {
+		cout <<"Usage: lab1 [string] \n";
+		return 0;
+	}
+	str = argv[1];
+	RemoveSpaces(str);
 	return 0;
 }
 
