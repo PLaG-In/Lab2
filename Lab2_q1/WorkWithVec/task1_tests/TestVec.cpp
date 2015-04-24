e9bee991055e5efc18a36e1a4a901777a5f6fdcb
PLaGInc <vladimir.alt@inbox.ru>
2015-04-24 08:42:36 +0400

    Тесты исправлены в задании 1

--- Lab2_q1/WorkWithVec/task1_tests/TestVec.cpp
+++ Lab2_q1/WorkWithVec/task1_tests/TestVec.cpp
@@ -18,30 +18,30 @@ BOOST_AUTO_TEST_CASE(ResultWithEmptyVector)
 	BOOST_CHECK(emptyVector.empty());
 }
 
-BOOST_AUTO_TEST_CASE(VectorWithMinElemEqZero)
+BOOST_AUTO_TEST_CASE(VectorWithMinElementZero)
 {
 	vector<float> numbers = { 4, 0, 3 };
 	ProcessVector(numbers);
 	BOOST_CHECK(VectorsAreEqual(numbers, { 0, 0, 0 }));
 }
 
-BOOST_AUTO_TEST_CASE(VectorWithoutPositivesDoesntChangeContent)
+BOOST_AUTO_TEST_CASE(VectorWithOneElement)
 {
-	vector<float> numbers = { -4, -1, -3 };
+	vector<float> numbers = { 3 };
 	ProcessVector(numbers);
-	BOOST_CHECK(VectorsAreEqual(numbers, { 4, 12, 16 }));
+	BOOST_CHECK(VectorsAreEqual(numbers, { 9 }));
 }
 
-BOOST_AUTO_TEST_CASE(VectorWithOnePositiveElement)
-{
-	vector<float> numbers = { -1, 3 };
-	ProcessVector(numbers);
-	BOOST_CHECK(VectorsAreEqual(numbers, { -3, 1 }));
-}
-
-BOOST_AUTO_TEST_CASE(VectorWithSeveralPositiveElements)
+BOOST_AUTO_TEST_CASE(VectorWithMinNegativeElement)
 {
 	vector<float> numbers = { -1, 1, 2, 3 };
 	ProcessVector(numbers);
 	BOOST_CHECK(VectorsAreEqual(numbers, { -3, -2, -1, 1 }));
+}
+
+BOOST_AUTO_TEST_CASE(VectorWithMinPositiveElement)
+{
+	vector<float> numbers = { 3, 6, 2, 4 };
+	ProcessVector(numbers);
+	BOOST_CHECK(VectorsAreEqual(numbers, { 4, 6, 8, 12 }));
 }
