be21abfea19b28aae14e375ff25931de506485ac
PLaGInc <vladimir.alt@inbox.ru>
2015-05-08 08:59:43 +0400

    Тесты подкорректированы

--- Lab2_q3/frequencyOfOccurrence/TestFreq/TestFreq.cpp
+++ Lab2_q3/frequencyOfOccurrence/TestFreq/TestFreq.cpp
@@ -18,21 +18,24 @@ BOOST_AUTO_TEST_CASE(ResultWithEmptyString)
 
 BOOST_AUTO_TEST_CASE(VectorWithLowerCaseStrings)
 {
-	string str = "new bee bee bew";
+	string str = "new bee bee bew new new";
 	map<string, int> dictOfWords = DetermineTheFrequency(str);
 	BOOST_CHECK(dictOfWords["bee"] == 2);
+	BOOST_CHECK(dictOfWords["new"] == 3);
 }
 
 BOOST_AUTO_TEST_CASE(VectorWithDifferentCasesStrings)
 {
-	string str = "new bee bee bew BEE bEE Bee feel";
+	string str = "new bee bee bew BEE nEw bEE Bee feel";
 	map<string, int> dictOfWords = DetermineTheFrequency(str);
 	BOOST_CHECK(dictOfWords["bee"] == 5);
+	BOOST_CHECK(dictOfWords["new"] == 2);
 }
 
 BOOST_AUTO_TEST_CASE(VectorWithHigherCaseStrings)
 {
-	string str = "NEW BEE BEE BEW KAZ'MODAN";
+	string str = "NEW BEE BEE BEW KAZ'MODAN NEW";
 	map<string, int> dictOfWords = DetermineTheFrequency(str);
 	BOOST_CHECK(dictOfWords["bee"] == 2);
+	BOOST_CHECK(dictOfWords["new"] == 2);
 }
